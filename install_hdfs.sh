5298d38e65096dc3595d30547eaaa36d85d71a17
root <root@node-1.config1.postman-pg0.wisc.cloudlab.us>
2019-01-14 23:39:05 -0700

    first commit

---
+++ install_hdfs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install vim
+
+sudo apt-get -y install software-properties-common
+echo | sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y install oracle-java8-installer
+sudo apt-get -y install maven
+sudo apt-get -y install build-essential autoconf automake libtool cmake zlib1g-dev pkg-config libssl-dev
+
+sudo apt-get -y install libprotobuf-java
+wget https://github.com/protocolbuffers/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
+tar zxvf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0/
+./configure
+make
+make check
+sudo make install
+cd ~
+
+# Optional packages
+sudo apt-get -y install snappy libsnappy-dev
+sudo apt-get -y install bzip2 libbz2-dev
+sudo apt-get -y install libjansson-dev
+sudo apt-get -y install fuse libfuse-dev
+sudo apt-get -y install zstd
+
+sudo ldconfig
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle/" >> ~/.bashrc
+source ~/.bashrc
+
+wget http://apache.claz.org/hadoop/common/hadoop-3.1.1/hadoop-3.1.1-src.tar.gz
+tar zxvf hadoop-3.1.1-src.tar.gz
+
+cd hadoop-3.1.1-src
---
+++ start_hdfs_with_conf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+type=$1
+property=$2
+node=4
+
+~/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1/sbin/stop-dfs.sh
+
+echo "hdfs stopped"
+
+for i in 0 1 2 3 4
+do
+    scp ~/config-inconsistency/hdfs/etc/* node-"$i"-link-0:~/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1/etc/hadoop
+    ssh node-"$i"-link-0 "rm -rf /root/data; mkdir /root/data; rm ~/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1/logs/*"
+done
+
+rm -rf /tmp/hadoop-root
+
+if [ "$#" -ne 2 ]
+then
+    echo "configuration type property unset, start with default conf"
+else
+    scp ~/config-inconsistency/hdfs/$type/$property/hdfs-site.xml node-"$node"-link-0:~/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1/etc/hadoop
+fi
+
+~/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1/bin/hdfs namenode -format
+~/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1/sbin/start-dfs.sh
