9daa645e2f465ff2091d77effaa41344a8e0cb28
root <root@node-0.namenode1.postman-pg0.clemson.cloudlab.us>
2019-02-18 00:34:48 -0500

    modify run_test.sh so that it takes name and value as arguments

--- hdfs/ha/sbin/run_test.sh
+++ hdfs/ha/sbin/run_test.sh
@@ -9,12 +9,23 @@ fi
 # it needs to load global variables
 . $TEST_HOME/sbin/global_var.sh
 
-if [ $# -lt 1 ]; then
-    echo "./run_test TESTDIR"
+if [ $# -lt 4 ]; then
+    echo "./run_test [name] [value] [round] [waittime]"
     exit
 fi
 
-testdir=$1
+name=$1
+value=$2
+round=$3
+waittime=$4
+
+# create test dir
+testdir="$TEST_HOME"/"$name"-"$value"-"$round"-"$waittime"
+mkdir $testdir
+cp $TEST_HOME/etc/hdfs-site-template.xml $testdir/hdfs-site.xml
+sed -i "s/nametobereplaced/$name/g" $testdir/hdfs-site.xml
+sed -i "s/valuetobereplaced/$value/g" $testdir/hdfs-site.xml
+exec &> $testdir/run.log
 
 # start the cluster
 $TEST_HOME/sbin/cluster_cmd.sh start
@@ -26,14 +37,14 @@ client_benchmark_main_pid=$!
 
 sleep 60
 
-for i in $(seq 1 3)
+for i in $(seq 1 $round)
 do
     # change configuration to be as given file
     $TEST_HOME/sbin/reconf.sh $testdir/hdfs-site.xml
-    sleep 600
+    sleep $waittime
     # change configuration to be as given file
     $TEST_HOME/sbin/reconf.sh $TEST_HOME/etc/hdfs-site.xml
-    sleep 600
+    sleep $waittime
 done
 
 # stop running benchmark
