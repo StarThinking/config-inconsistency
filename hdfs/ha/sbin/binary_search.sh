b2cfe8c15827744e327cc956a646d3db4830ad90
root <root@node-0.hadoop-config4.postman-pg0.clemson.cloudlab.us>
2019-03-21 00:36:01 -0400

    refine binary search

--- hdfs/ha/sbin/binary_search.sh
+++ hdfs/ha/sbin/binary_search.sh
@@ -9,15 +9,23 @@ fi
 # it needs to load global variables
 . $TEST_HOME/sbin/global_var.sh
 
-if [ "$#" -ne 3 ]
+if [ "$#" -lt 5 ]
 then
-    echo "./binary_search.sh [name] [reconf_type] [time_senestive]"
+    echo "./binary_search.sh [name] [reconf_type] [round] [waittime] [time_senestive] [read_times] [benchmark_threads]" 
     exit
 fi
 
 name=$1
 reconf_type=$2
-time_sense=$3
+round=$3
+waittime=$4
+time_sense=$5
+read_times=10 # default value
+benchmark_threads=5 # default value
+if [ $# -eq 8 ]; then
+    read_times=$6
+    benchmark_threads=$7
+fi
 
 function verify_input {
     local value=$1
@@ -29,12 +37,12 @@ function verify_input {
     local waittime=30
     local ret=0 # 0 if ok, 1 if not ok
     local read_times=10
-    local benchmark_threads=5
+    local benchmark_threads=6
 
     echo "verify_input for parameter $name with value $value"
 
     $TEST_HOME/sbin/run_test.sh $name $value $reconf_type $test_mode $round $waittime $read_times $benchmark_threads
-    testdir="$TEST_HOME"/"$name"-"$value"-"$reconf_type"-"$test_mode"-"$round"-"$waittime"
+    local testdir="$TEST_HOME"/"$name"-"$value"-"$reconf_type"-"$test_mode"-"$round"-"$waittime"
    
     $TEST_HOME/sbin/verify_result.sh $testdir $reconf_type 
     ret=$?
@@ -42,17 +50,18 @@ function verify_input {
     return $ret   
 }
 
-retval=0
+retval=0 # pass return value to caller, as it can be larger than 255
 function find_minimum {
     local range_start=1
     local range_end=1
+    local lowest=1
 
-    # find range_start and range_end roughly
+    # find range first
     for (( ; ; ))
     do
         verify_input $range_end
 	if [ $? -eq 0 ]; then # might be too large
-	    echo "set range_end as $range_end"
+	    echo "break, set range_end as $range_end"
 	    break
 	else # ret=1: too small, double
 	    range_start=$range_end
@@ -60,36 +69,25 @@ function find_minimum {
 	fi
     done
 
-    #if [ $time_sense -eq 1 ]; then
-    #	lowest=$range_end
-    #	echo "time sensitive is true, return $lowest"
-    #	retval=$lowest
-    #	return
-    #fi
-
-    local lowest=$(( (range_start + range_end ) / 2 ))
-    if [ $range_start -eq $range_end ]; then
-	retval=$lowest	
-	return
-    fi	
-    
     # find the lowest value in the range
     local max_step=5
     local steps=0
     for (( ; ; ))
     do
+        lowest=$(( (range_start + range_end ) / 2 ))
+	
+	if [ $range_start -ge $range_end ] || [ $steps -gt $max_step ]; then 
+	    break
+	fi
+
         verify_input $lowest
+        
         if [ $? -eq 0 ]; then # ok-branch: might be too large
-    	    local distance=$(( range_end - range_start ))
-    	    if [ $distance -le 2 ] || [ $steps -gt $max_step ] ; then
-    	    	break
-    	    else
-    	        range_end=$lowest
-    	        lowest=$(( ( range_end + range_start ) / 2 ))
-    	    fi
+ 	    echo "ok branch"
+    	    range_end=$lowest
         else # not-ok-branch: too small
-    	    range_start=$lowest
-    	    lowest=$(( (range_end + range_start) / 2 ))
+	    echo "not ok branch"
+    	    range_start=$(( lowest + 1 )) # it is important to add by 1
         fi
 	steps=$(( steps + 1 ))
     done
@@ -98,8 +96,12 @@ function find_minimum {
     return 
 }
 
+echo > $TEST_HOME/bs_run.log
+exec &> $TEST_HOME/bs_run.log
 find_minimum
 minimum_value=$retval
 echo "the minimum value for parameter $name is $minimum_value"
-#$TEST_HOME/sbin/run_test.sh $name $minimum_value namenode default 1 300 10 5
+$TEST_HOME/sbin/run_test.sh $name $minimum_value $reconf_type default $round $waittime
+testdir="$TEST_HOME"/"$name"-"$minimum_value"-"$reconf_type"-"default"-"$round"-"$waittime"
+mv $TEST_HOME/bs_run.log $testdir
 #$TEST_HOME/sbin/run_test.sh $name $minimum_value namenode test 1 300 10 5
