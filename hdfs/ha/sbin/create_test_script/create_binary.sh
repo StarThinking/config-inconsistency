5b32c55cf9a33eddb97cafc18a2ac596dad61c7e
root <root@node-0.hadoop-config1.postman-pg0.clemson.cloudlab.us>
2019-03-25 02:25:38 -0400

    modify binary

---
+++ hdfs/ha/sbin/create_test_script/create_binary.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#./binary_search.sh [name] [reconf_type] [round] [waittime] [time_senestive] [read_times] [benchmark_threads]
+
+line_num=$(wc -l name.txt | cut -d' ' -f1)
+
+for i in $(seq 1 $line_num)
+do
+    echo -n "./sbin/binary_search.sh " >> tmp.sh 
+    echo -n `sed -n "$i p" name.txt` >> tmp.sh
+    echo -n " " >> tmp.sh
+    echo -n " namenode" >> tmp.sh
+    echo -n " 2" >> tmp.sh
+    echo -n " 300" >> tmp.sh
+    echo " 1" >> tmp.sh
+    
+    echo ""  >> tmp.sh
+done
+
