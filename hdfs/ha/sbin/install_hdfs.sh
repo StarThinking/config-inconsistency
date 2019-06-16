825463a1a1c98c55baed236d645c42db98c49f41
root <root@node-0.hdfs.postman-pg0.wisc.cloudlab.us>
2019-06-15 22:37:24 -0600

    update java install

--- hdfs/ha/sbin/install_hdfs.sh
+++ hdfs/ha/sbin/install_hdfs.sh
@@ -5,12 +5,11 @@ sudo apt-get -y upgrade
 sudo apt-get -y install vim
 
 sudo apt-get -y install software-properties-common
-echo | sudo add-apt-repository ppa:webupd8team/java
+sudo install openjdk-8-jdk
 sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
 sudo apt-get -y install maven
 sudo apt-get -y install build-essential autoconf automake libtool cmake zlib1g-dev pkg-config libssl-dev
-
+#
 sudo apt-get -y install libprotobuf-java
 wget https://github.com/protocolbuffers/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
 tar zxvf protobuf-2.5.0.tar.gz
@@ -29,13 +28,13 @@ sudo apt-get -y install fuse libfuse-dev
 sudo apt-get -y install zstd
 
 sudo ldconfig
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle/" >> ~/.profile
-echo "export HADOOP_HOME=/root/hadoop-3.1.1-src/hadoop-dist/target/hadoop-3.1.1" >> ~/.profile
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.profile
+echo "export HADOOP_HOME=/root/hadoop-3.1.2-src/hadoop-dist/target/hadoop-3.1.2" >> ~/.profile
 
-wget http://apache.claz.org/hadoop/common/hadoop-3.1.1/hadoop-3.1.1-src.tar.gz
-tar zxvf hadoop-3.1.1-src.tar.gz
+wget http://apache.claz.org/hadoop/common/hadoop-3.1.2/hadoop-3.1.2-src.tar.gz
+tar zxvf hadoop-3.1.2-src.tar.gz
 
-cd hadoop-3.1.1-src
+cd hadoop-3.1.2-src
 mvn package -Pdist,native -DskipTests -Dtar
 
 # install zooleeper
