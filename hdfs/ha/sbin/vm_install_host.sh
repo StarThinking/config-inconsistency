5d5114ded83b961907fafe2cee04f6db24a1306f
root <root@node-0.hdfs-clemson.postman-pg0.clemson.cloudlab.us>
2019-07-24 20:45:47 -0600

    qemu permission issue

--- hdfs/ha/sbin/vm_install_host.sh
+++ hdfs/ha/sbin/vm_install_host.sh
@@ -10,16 +10,19 @@ echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64" >> ~/.profile
 echo "export HADOOP_HOME=/root/hadoop-3.1.2-src/hadoop-dist/target/hadoop-3.1.2" >> ~/.profile
 
 echo "export ZOOKEEPER_HOME=/root/zookeeper-3.4.13" >> ~/.profile
-echo "export TEST_HOME=/root/config-inconsistency/hdfs/ha" >> ~/.profile
+TEST_HOME=/root/config-inconsistency/hdfs/ha
+echo "export TEST_HOME=$TEST_HOME" >> ~/.profile
 
 # install virsh
 sudo apt install -y qemu-kvm libvirt0 libvirt-bin virt-manager bridge-utils
 sudo systemctl enable libvirt-bin
 sudo apt-get -y install expect
+ 
+# allow root user for qemu
+cp $TEST_HOME/sbin/qemu.conf /etc/libvirt/qemu.conf
 
-printf '%s\n' n '' '' '' '' w | sudo fdisk /dev/sdb
-sudo mkfs.ext4 /dev/sdb1
-mkdir /root/vm_images
-sudo mount /dev/sdb1 /root/vm_images
+#printf '%s\n' n '' '' '' '' w | sudo fdisk /dev/sdb
+#sudo mkfs.ext4 /dev/sdb1
+#mkdir /root/vm_images
+#sudo mount /dev/sdb1 /root/vm_images
 
-chown -R libvirt-qemu /root
