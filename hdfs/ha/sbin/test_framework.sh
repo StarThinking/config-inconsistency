13b39a75e5059c3f97fa7075e555f7e414481a0c
StarThinking <ma.1189@osu.edu>
2019-08-25 16:41:50 -0400

    update

--- hdfs/ha/sbin/test_framework.sh
+++ hdfs/ha/sbin/test_framework.sh
@@ -19,10 +19,10 @@ for line in $(cat < "$task_file")
 do
     echo "---------------------------------------------------------"
     stop=0 
-    component=$(echo $line | awk -F " " '{print $1}')
-    parameter=$(echo $line | awk -F " " '{print $2}')
-    value1=$(echo $line | awk -F " " '{print $3}')
-    value2=$(echo $line | awk -F " " '{print $4}')
+    component=$(echo $line | awk -F '[#| ]' '{print $1}')
+    parameter=$(echo $line | awk -F '[#| ]' '{print $2}')
+    value1=$(echo $line | awk -F '[#| ]' '{print $3}')
+    value2=$(echo $line | awk -F '[#| ]' '{print $4}')
     tuple_dir="$component""$split""$parameter""$split""$value1""$split""$value2"
     echo component=$component parameter=$parameter value1=$value1 value2=$value2
     mkdir $tuple_dir 
@@ -31,6 +31,10 @@ do
     echo "run online v1-v2 test"
     reconfig_mode=online_reconfig
     $TEST_HOME/sbin/run_hdfs_test.sh $component $parameter $value1 $value2 $reconfig_mode $waittime 
+    if [ $? -ne 0 ]; then
+        echo "error during online v1-v2 test, quit"
+        stop=1
+    fi
     online_12="$component""$split""$parameter""$split""$value1""$split""$value2""$split""$reconfig_mode""$split""$waittime"
     subsetof $online_12
     ret=$?
