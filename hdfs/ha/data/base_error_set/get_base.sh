677d6db1e79e6c98d021ab51040c81f83660c9b6
StarThinking <ma.1189@osu.edu>
2019-09-11 23:59:35 -0400

    update

--- hdfs/ha/data/base_error_set/get_base.sh
+++ hdfs/ha/data/base_error_set/get_base.sh
@@ -5,21 +5,15 @@ if [ -z "$TEST_HOME" ]; then
     exit 3
 fi
 
-for i in 100 200 400 800
+for i in 100 200 400 1600
 do
 $TEST_HOME/sbin/run_hdfs_test.sh namenode dfs.image.compress false false online_reconfig $i
 $TEST_HOME/sbin/run_hdfs_test.sh namenode dfs.image.compress false false cluster_stop $i
 done 
 
-#for i in 30 31 32 33 34 35 36 37 38 39 40 60 61 62 120 200 2000
+#for i in 60 61 62 63 64 65 66 67 68 69 70 120 200 400 800 1000 1600
 #do
-#$TEST_HOME/sbin/run_hdfs_test.sh datanode dfs.image.compress false false online_reconfig $i
-#$TEST_HOME/sbin/run_hdfs_test.sh datanode dfs.image.compress false false cluster_stop $i
-#done 
-#
-#for i in 30 31 32 33 34 35 36 37 38 39 40 60 61 62 120 200 2000
-#do
-#$TEST_HOME/run_hdfs_test.sh journalnode dfs.image.compress false false online_reconfig $i
-#$TEST_HOME/run_hdfs_test.sh journalnode dfs.image.compress false false cluster_stop $i
+#$TEST_HOME/sbin/run_hdfs_test.sh namenode dfs.image.compress false false online_reconfig $i
+#$TEST_HOME/sbin/run_hdfs_test.sh namenode dfs.image.compress false false cluster_stop $i
 #done 
 
