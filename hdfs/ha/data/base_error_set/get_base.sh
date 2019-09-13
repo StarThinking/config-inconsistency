15244f1a77f245d7a5f019dbb80423ef104b42fc
StarThinking <ma.1189@osu.edu>
2019-09-13 00:50:57 -0400

    update

--- hdfs/ha/data/base_error_set/get_base.sh
+++ hdfs/ha/data/base_error_set/get_base.sh
@@ -7,8 +7,14 @@ fi
 
 for i in 50 100 200 400 1000
 do
-$TEST_HOME/sbin/run_hdfs_test.sh namenode dfs.image.compress false false online_reconfig $i
-$TEST_HOME/sbin/run_hdfs_test.sh namenode dfs.image.compress false false cluster_stop $i
+$TEST_HOME/sbin/run_hdfs_test.sh datanode dfs.image.compress false false online_reconfig $i
+$TEST_HOME/sbin/run_hdfs_test.sh datanode dfs.image.compress false false cluster_stop $i
+done 
+
+for i in 50 100 200 400 1000
+do
+$TEST_HOME/sbin/run_hdfs_test.sh journalnode dfs.image.compress false false online_reconfig $i
+$TEST_HOME/sbin/run_hdfs_test.sh journalnode dfs.image.compress false false cluster_stop $i
 done 
 
 #for i in 60 61 62 63 64 65 66 67 68 69 70 120 200 400 800 1000 1600
