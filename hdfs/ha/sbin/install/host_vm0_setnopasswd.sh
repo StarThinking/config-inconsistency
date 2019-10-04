c60b670a1ecb46a0d9484b4bc946711110b7989e
root <root@node-2.hdfs-clemson.postman-pg0.clemson.cloudlab.us>
2019-10-03 22:47:14 -0400

    a bug

--- hdfs/ha/sbin/install/host_vm0_setnopasswd.sh
+++ hdfs/ha/sbin/install/host_vm0_setnopasswd.sh
@@ -11,7 +11,7 @@ $TEST_HOME/sbin/util/vm_autoscp.sh root $vm0_ip ~/.ssh/id_rsa.pub ~/id_rsa.pub.t
 $TEST_HOME/sbin/util/vm_autossh.sh root $vm0_ip "cat ~/id_rsa.pub.tmp >> ~/.ssh/authorized_keys; rm ~/id_rsa.pub.tmp"
 sleep 2
 echo "testing host-vm0 ssh without password:"
-msg=$($ssh $vm0_ip "cat /etc/hostname")
+msg=$(ssh $vm0_ip "cat /etc/hostname")
 if [ "$msg" == "node-0-link-0" ]; then
     echo "Ok"
 else
