4ab06b48770ab41a2e96a8449c226d79193580c2
root <root@node-0.hdfs-clemson.postman-pg0.clemson.cloudlab.us>
2019-10-05 17:05:01 -0400

    multiplex cmd_master and cmd_slave

--- hdfs/ha/sbin/cmd_master.sh
+++ hdfs/ha/sbin/cmd_master.sh
@@ -6,34 +6,52 @@ if [ -z "$TEST_HOME" ]; then
     exit 3
 fi
 
-if [ $# -lt 5 ]; then
-    echo "args are wrong: [test_id] [task_file] [wait_time] [repeat_times] [nodes..], exit"
-    exit 1
+function start_test {
+    if [ $# -lt 5 ]; then
+        echo "args are wrong: [test_id] [task_file] [wait_time] [repeat_times] [nodes..], exit"
+        exit 1
+    fi
+    
+    test_id=$1
+    task_file=$2
+    wait_time=$3
+    repeat_times=$4
+    shift 4
+    nodes=("$@")
+    
+    
+    echo "task_file is $task_file"
+    nodes_size=${#nodes[@]}
+    echo "nodes are ${nodes[@]}, nodes_size is $nodes_size"
+    
+    split $task_file -d -n l/$nodes_size
+    sub_task_files=($(ls | grep x0 | sort -u))
+    echo "sub_task_files is ${sub_task_files[@]}"
+    
+    for (( i=0; i<nodes_size; i++ ))
+    do
+        node_id=${nodes[i]}
+        ssh node-"$node_id" "cd $TEST_HOME; git pull"
+    done
+    
+    echo "finished node git pull"
+    
+    for (( i=0; i<nodes_size; i++ ))
+    do
+        node_id=${nodes[i]}
+        sub_task_file=$(pwd)/${sub_task_files[i]}
+        echo "sub_task_file for node-$node_id is:"
+        cat $sub_task_file
+        ssh node-"$node_id" "$TEST_HOME/sbin/cmd_slave.sh 'start_test' $test_id $node_id $sub_task_file $wait_time $repeat_times"
+        echo "cmd slave has been called for node-$node_id"  
+        echo "" 
+    done
+}
+
+cmd=$1
+shift 1
+if [ "$cmd" == 'start_test' ] || [ "$cmd" == 'collect_result' ]; then
+    $cmd $@
+else
+    echo "ERROR: wrong cmd $cmd"
 fi
-
-test_id=$1
-task_file=$2
-wait_time=30
-repeat_times=1
-shift 4
-nodes=("$@")
-
-
-echo "task_file is $task_file"
-nodes_size=${#nodes[@]}
-echo "nodes are ${nodes[@]}, nodes_size is $nodes_size"
-
-split $task_file -d -n l/$nodes_size
-split_prefix='x0'
-
-for i in ${nodes[@]}
-do
-    task_file=$(pwd)/"$split_prefix""$i"
-    echo "task for node-$i is:"
-    cat $task_file
-    ssh node-"$i" "cd $TEST_HOME; git pull"
-    ssh node-"$i" "$TEST_HOME/sbin/cmd_slave.sh $test_id $i $task_file $wait_time $repeat_times"
-    echo "cmd slave has been called for node-$i"  
-    echo "" 
-done
-
--- hdfs/ha/sbin/cmd_slave.sh
+++ hdfs/ha/sbin/cmd_slave.sh
@@ -8,38 +8,54 @@ if [ -z "$TEST_HOME" ]; then
     exit 3
 fi
 
-if [ $# -ne 5 ]; then
+cmd_master='node-0'
+
+function start_test {
+    if [ $# -ne 5 ]; then
+        echo "ERROR: wrong args"
+        exit 1
+    fi
+
+    test_id=$1 # based on date and time
+    node_id=$2
+    task_file=$3
+    wait_time=$4
+    repeat_times=$5
+    
+    test_dir=~/"$test_id"-node-$node_id
+    mkdir $test_dir
+    
+    vm0=$(virsh domifaddr node-0-link-0 | grep ipv4 | awk -F " " '{print $4}' | cut -d"/" -f1)
+    echo vm0 is $vm0
+    
+    # fetch task from cmd_master
+    scp $cmd_master:$task_file $test_dir/task.txt
+    if [ $? -ne 0 ]; then
+        echo "ERROR: fetch task from cmd_master failed! quit"
+        exit 4
+    fi
+    
+    echo "send test_dir $test_dir to vm0"
+    scp -r $test_dir $vm0:~
+    rm -rf $test_dir
+    
+    echo "git pull for vm0 on node-$node_id"
+    ssh $vm0 "cd $TEST_HOME; git pull"
+    
+    echo "starting test_framework..."
+    # hard-coded wait time and repeat times
+    nohup ssh $vm0 "cd $test_dir; export wait_time=$wait_time; export repeat_times=$repeat_times; nohup bash --login $TEST_HOME/sbin/test_framework.sh ./task.txt $wait_time $repeat_times >> nohup.txt &" &
+}
+
+if [ $# -lt 2 ]; then
     echo "ERROR: wrong args, quit"
     exit 2
 fi
 
-cmd_master='node-0'
-test_id=$1 # based on date and time
-node_id=$2
-task_file=$3
-wait_time=$4
-repeat_times=$5
-
-test_dir=~/"$test_id"-node-$node_id
-mkdir $test_dir
-
-vm0=$(virsh domifaddr node-0-link-0 | grep ipv4 | awk -F " " '{print $4}' | cut -d"/" -f1)
-echo vm0 is $vm0
-
-# fetch task from cmd_master
-scp $cmd_master:$task_file $test_dir/task.txt
-if [ $? -ne 0 ]; then
-    echo "ERROR: fetch task from cmd_master failed! quit"
-    exit 4
+cmd=$1
+shift 1
+if [ "$cmd" == 'start_test' ] || [ "$cmd" == 'collect_result' ]; then
+    $cmd $@
+else
+    echo "ERROR: wrong cmd $cmd"
 fi
-
-echo "send test_dir $test_dir to vm0"
-scp -r $test_dir $vm0:~
-rm -rf $test_dir
-
-echo "git pull for vm0 on node-$node_id"
-ssh $vm0 "cd $TEST_HOME; git pull"
-
-echo "starting test_framework..."
-# hard-coded wait time and repeat times
-nohup ssh $vm0 "cd $test_dir; export wait_time=$wait_time; export repeat_times=$repeat_times; nohup bash --login $TEST_HOME/sbin/test_framework.sh ./task.txt $wait_time $repeat_times >> nohup.txt &" &
